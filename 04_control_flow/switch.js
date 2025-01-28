d18d8775d77abecc14fc845dd1d7dd37b7682966
Umesh <97879415+Umesh7428@users.noreply.github.com>
2025-01-28 09:10:07 +0000

    about if/else and loops

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch(month){
+    case "jan":
+        console.log("Birthday coming");
+        break;
+    case "march":
+        console.log("Vivek's Birthday coming");
+        break;
+    case "aug":
+        console.log("shubham's Birthday coming");
+        break;
+    case "sept":
+        console.log("sachin's Birthday coming");
+        break;
+        default:
+            console.log("none");
+            break; 
+}
