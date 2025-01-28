0674705a4ba8c18b14951e54e31b3b6afbdc460c
Umesh <97879415+Umesh7428@users.noreply.github.com>
2025-01-28 09:10:28 +0000

    ..

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+
+// let i = 0
+// while(i <= 10){
+//   console.log(`Value of i is ${i}`);
+
+//   i+=2
+
+// }
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    
+    arr++
+}
+
+let score = 11
+
+do{
+    console.log(score);
+    score++
+
+}while(score <= 10);
